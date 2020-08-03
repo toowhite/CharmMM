027314d5c1c74e795017e03bb765af5fb354c7c6
Zhuowei Li <zli58@ncsu.edu>
2020-08-03 12:25:28 -0400

    Add sample wallpapers && support center mode

--- app.js
+++ app.js
@@ -39,21 +39,43 @@ function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
 
+function getAdjustmentParameters(picWidth, picHeight, displayWidth, displayHeight, mode) {
+    let params = {}
+    if (mode == 'center') {
+        let scale = Math.max(displayWidth / picWidth, displayHeight / picHeight);
+        params.x = (picWidth * scale - displayWidth) / 2;
+        params.y = (picHeight * scale - displayHeight) / 2;
+        params.w = displayWidth;
+        params.h = displayHeight;
+        params.scale = scale;
+    }
+
+    return params;
+}
+
 async function generateWallpaper(size, displays) {
     
     let image = new Jimp(size.w, size.h, config.BackgroundColor);
 
-    let pics = [];
+    let picks = [];
     for (let display of displays) {
-        let pic;
+        let picked;
         do {
             let ratio = display.currentResX / display.currentResY;
-            let horizontalDisplay = ratio > 1.6 && ratio < 1.8;
-            pic = randomPick(horizontalDisplay);
-        } while (pics.includes(pic));
-        pics.push(pic);
-        let picData = await Jimp.read(path.join(config.PictureFolder, pic));
-        image.blit(picData, display.positionX, display.positionY, 0, 0, display.currentResX, display.currentResY);
+            let landscapeDisplay = landscapeRatio(ratio);
+            picked = randomPick(landscapeDisplay, display.currentResX, display.currentResY);
+        } while (picks.includes(picked));
+        picks.push(picked);
+        let picData = await Jimp.read(path.join(config.PictureFolder, picked.file));
+        if (config.FitMode == "center") {
+            let cp = getAdjustmentParameters(picked.correctedWidth, picked.correctedHeight, display.currentResX, display.currentResY, config.FitMode);
+            picData.scale(cp.scale);
+            image.blit(picData, display.positionX, display.positionY, cp.x, cp.y, cp.w, cp.h);
+        }
+        else {
+            throw new Error("Unsupported fit mode");
+        }
+        
     }
     //console.log(pics);
 
@@ -62,29 +84,41 @@ async function generateWallpaper(size, displays) {
     return fullpath;
 }
 
-function isHorizontal(size) {
+function isLandscapePic(size) {
     let ratio = size.width / size.height;
     if (size.hasOwnProperty("orientation")) {
-        return 1.6 < ratio && ratio < 1.8 && [1, 2, 3, 4].includes(size.orientation);
+        return landscapeRatio(ratio) && [1, 2, 3, 4].includes(size.orientation);
     }
     else {
-        return 1.6 < ratio && ratio < 1.8;
+        return landscapeRatio(ratio);
     }
 }
 
-function randomPick(pickHorizonal = true) {
+function landscapeRatio(ratio) {
+    return 1.2 < ratio && ratio < 2;
+}
+
+function randomPick(pickLandscape, minWidth, minHeight) {
     let files = fs.readdirSync(config.PictureFolder);
     shuffle(files);
     //console.log(files);
     for (let file of files) {
         let size = sizeOf(path.join(config.PictureFolder, file));
 
-        let horizontal = isHorizontal(size);
-        if (pickHorizonal && horizontal) {
-            return file;
+        let landscapePic = isLandscapePic(size);
+        if (pickLandscape && landscapePic) {
+            let w = Math.max(size.width, size.height);
+            let h = Math.min(size.width, size.height);
+            if (w >= minWidth && h >= minHeight) {
+                return { file: file, correctedWidth: w, correctedHeight: h };
+            }
         }
-        else if (!pickHorizonal && !horizontal) {
-            return file;
+        else if (!pickLandscape && !landscapePic) {
+            let w = Math.min(size.width, size.height);
+            let h = Math.max(size.width, size.height);
+            if (w >= minWidth && h >= minHeight) {
+                return { file: file, correctedWidth: w, correctedHeight: h };
+            }
         }
     }
     throw new Error('No available image');
