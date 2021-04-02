e5773daf0214111631adc793703cb73cdaa08e20
Zhuowei Li <zli58@ncsu.edu>
2021-04-02 09:40:14 +0800

    Remove VS; add linter

--- app.js
+++ app.js
@@ -1,148 +1,140 @@
 'use strict';
 
-const si = require('systeminformation');
-const Jimp = require('jimp');
-const fs = require('fs');
-const sizeOf = require('image-size');
-const path = require('path');
-const wp = require('wallpaper');
-const config = require('./config.json');
+import {graphics as _graphics} from 'systeminformation';
+import Jimp, {read} from 'jimp';
+import {readdirSync} from 'fs';
+import sizeOf from 'image-size';
+import {join} from 'path';
+import {set} from 'wallpaper';
+import {BackgroundColor, PictureFolder, FitMode} from './config.json';
 
 function getWallpaperSizeAndFixPositions(displays) {
-    
-    let height = 0;
-    let width = 0;
-    let minPosX = Infinity;
-    let minPosY = Infinity;
-    for (let display of displays) {
-        if (display.positionX < minPosX) {
-            minPosX = display.positionX;
-        }
-        if (display.positionY < minPosY) {
-            minPosY = display.positionY;
-        }
-        if (display.currentResY > height) {
-            height = display.currentResY;
-        }
-        width += display.currentResX;
+  let height = 0;
+  let width = 0;
+  let minPosX = Infinity;
+  let minPosY = Infinity;
+  for (const display of displays) {
+    if (display.positionX < minPosX) {
+      minPosX = display.positionX;
     }
-
-    for (let display of displays) {
-        display.positionX -= minPosX;
-        display.positionY -= minPosY;
+    if (display.positionY < minPosY) {
+      minPosY = display.positionY;
     }
+    if (display.currentResY > height) {
+      height = display.currentResY;
+    }
+    width += display.currentResX;
+  }
 
-    return { h: height, w: width };
+  for (const display of displays) {
+    display.positionX -= minPosX;
+    display.positionY -= minPosY;
+  }
+
+  return {h: height, w: width};
 }
 
 function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+  array.sort(() => Math.random() - 0.5);
 }
 
 function getAdjustmentParameters(picWidth, picHeight, displayWidth, displayHeight, mode) {
-    let params = {}
-    if (mode == 'center') {
-        let scale = Math.max(displayWidth / picWidth, displayHeight / picHeight);
-        if (scale >= 0.9) scale = 1;
-        params.x = (picWidth * scale - displayWidth) / 2;
-        params.y = (picHeight * scale - displayHeight) / 2;
-        params.w = displayWidth;
-        params.h = displayHeight;
-        params.scale = scale;
-    }
+  const params = {};
+  if (mode == 'center') {
+    let scale = Math.max(displayWidth / picWidth, displayHeight / picHeight);
+    if (scale >= 0.9) scale = 1;
+    params.x = (picWidth * scale - displayWidth) / 2;
+    params.y = (picHeight * scale - displayHeight) / 2;
+    params.w = displayWidth;
+    params.h = displayHeight;
+    params.scale = scale;
+  }
 
-    return params;
+  return params;
 }
 
 async function generateWallpaper(size, displays) {
-    
-    let image = new Jimp(size.w, size.h, config.BackgroundColor);
+  const image = new Jimp(size.w, size.h, BackgroundColor);
 
-    let picks = [];
-    for (let display of displays) {
-        let picked;
-        do {
-            let ratio = display.currentResX / display.currentResY;
-            let landscapeDisplay = landscapeRatio(ratio);
-            picked = randomPick(landscapeDisplay, display.currentResX, display.currentResY);
-        } while (picks.includes(picked));
-        picks.push(picked);
-        let picData = await Jimp.read(path.join(config.PictureFolder, picked.file));
-        if (config.FitMode == "center") {
-            let cp = getAdjustmentParameters(picked.correctedWidth, picked.correctedHeight, display.currentResX, display.currentResY, config.FitMode);
-            if (cp.scale != 1) {
-                picData.scale(cp.scale);
-            }
-            image.blit(picData, display.positionX, display.positionY, cp.x, cp.y, cp.w, cp.h);
-        }
-        else {
-            throw new Error("Unsupported fit mode");
-        }
-        
+  const picks = [];
+  for (const display of displays) {
+    let picked;
+    do {
+      const ratio = display.currentResX / display.currentResY;
+      const landscapeDisplay = landscapeRatio(ratio);
+      picked = randomPick(landscapeDisplay, display.currentResX, display.currentResY);
+    } while (picks.includes(picked));
+    picks.push(picked);
+    const picData = await read(join(PictureFolder, picked.file));
+    if (FitMode == 'center') {
+      const cp = getAdjustmentParameters(picked.correctedWidth, picked.correctedHeight, display.currentResX, display.currentResY, FitMode);
+      if (cp.scale != 1) {
+        picData.scale(cp.scale);
+      }
+      image.blit(picData, display.positionX, display.positionY, cp.x, cp.y, cp.w, cp.h);
+    } else {
+      throw new Error('Unsupported fit mode');
     }
-    //console.log(pics);
+  }
+  // console.log(pics);
 
-    let fullpath = path.join(__dirname, "_tmp.jpg");
-    image.write(fullpath);
-    return fullpath;
+  const fullpath = join(__dirname, '_tmp.jpg');
+  image.write(fullpath);
+  return fullpath;
 }
 
 function isLandscapePic(size) {
-    let ratio = size.width / size.height;
-    if (size.hasOwnProperty("orientation")) {
-        return landscapeRatio(ratio) && [1, 2, 3, 4].includes(size.orientation);
-    }
-    else {
-        return landscapeRatio(ratio);
-    }
+  const ratio = size.width / size.height;
+  if (size.hasOwnProperty('orientation')) {
+    return landscapeRatio(ratio) && [1, 2, 3, 4].includes(size.orientation);
+  } else {
+    return landscapeRatio(ratio);
+  }
 }
 
 function landscapeRatio(ratio) {
-    return 1.2 < ratio && ratio < 2;
+  return 1.2 < ratio && ratio < 2;
 }
 
 function randomPick(pickLandscape, minWidth, minHeight) {
-    let files = fs.readdirSync(config.PictureFolder);
-    shuffle(files);
-    //console.log(files);
-    for (let file of files) {
-        let size = sizeOf(path.join(config.PictureFolder, file));
+  const files = readdirSync(PictureFolder);
+  shuffle(files);
+  // console.log(files);
+  for (const file of files) {
+    const size = sizeOf(join(PictureFolder, file));
 
-        let landscapePic = isLandscapePic(size);
-        if (pickLandscape && landscapePic) {
-            let w = Math.max(size.width, size.height);
-            let h = Math.min(size.width, size.height);
-            if (w >= minWidth * 0.9 && h >= minHeight * 0.9) {
-                return { file: file, correctedWidth: w, correctedHeight: h };
-            }
-        }
-        else if (!pickLandscape && !landscapePic) {
-            let w = Math.min(size.width, size.height);
-            let h = Math.max(size.width, size.height);
-            if (w >= minWidth * 0.9 && h >= minHeight * 0.9) {
-                return { file: file, correctedWidth: w, correctedHeight: h };
-            }
-        }
+    const landscapePic = isLandscapePic(size);
+    if (pickLandscape && landscapePic) {
+      const w = Math.max(size.width, size.height);
+      const h = Math.min(size.width, size.height);
+      if (w >= minWidth * 0.9 && h >= minHeight * 0.9) {
+        return {file: file, correctedWidth: w, correctedHeight: h};
+      }
+    } else if (!pickLandscape && !landscapePic) {
+      const w = Math.min(size.width, size.height);
+      const h = Math.max(size.width, size.height);
+      if (w >= minWidth * 0.9 && h >= minHeight * 0.9) {
+        return {file: file, correctedWidth: w, correctedHeight: h};
+      }
     }
-    throw new Error('No available image');
+  }
+  throw new Error('No available image');
 }
 
-process.on('uncaughtException', function (err) {
-    console.error(err);
-    process.exit(1);
+process.on('uncaughtException', function(err) {
+  console.error(err);
+  process.exit(1);
 });
 
 
 (async () => {
-    let graphics = await si.graphics();
-    let displays = graphics.displays;
-    let size = getWallpaperSizeAndFixPositions(displays);
-    let tmpFilepath = await generateWallpaper(size, displays);
+  const graphics = await _graphics();
+  const displays = graphics.displays;
+  const size = getWallpaperSizeAndFixPositions(displays);
+  const tmpFilepath = await generateWallpaper(size, displays);
 
-    console.log(displays);
-    wp.set(tmpFilepath);
+  console.log(displays);
+  set(tmpFilepath);
 })();
-
-
 
 
